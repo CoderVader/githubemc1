dd1a8cf5ea483a00c88eba8806b8d903527b9b87
CoderVader <59097511+CoderVader@users.noreply.github.com>
2024-09-01 16:58:00 +0200

    test.py modified in firstBranch

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 print("This is a simple commit test")
 print("The point is to update this file and push to the branch and not Main")
 print("Lets get started!")
+
+print("Now I have switched to the firstBranch from main")
+print("Now I am adding these lines in the firstBranch")
+print("These lines are only available in firstBranch")
